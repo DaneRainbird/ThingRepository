96f0e88f3e9e052ce9fa0e801ef3db0462c97f53
drai0001 <13450157+Datenyan@users.noreply.github.com>
2020-12-07 00:13:28 +1100

    Added Auth0 authentication

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let express = require("express");
 let bodyParser = require("body-parser"); 
 let dotEnv = require("dotenv");
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 let app = express();
 
 // Configure imports
@@ -13,9 +15,22 @@ app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({extended: false})) 
 app.use(express.static('res'));
 
+// Setup Auth0 authentication
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SESSSECRET,
+    baseURL: 'http://localhost:8080',
+    clientID: '7ENARlurk445mBHNTHxPtbH9dDk7AHSp',
+    issuerBaseURL: 'https://dev-4fzaj737.au.auth0.com'
+};
+ 
+// Auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
 // Setup router for endpoints
-let router = require("./router.js");
-app.use("/", router); 
+var router = require("./router.js");
+app.use('/', router);
 
 // Run the server 
 app.listen(process.env.PORT, () => console.log("Server running at port " + process.env.PORT));
--- router.js
+++ router.js
@@ -1,13 +1,53 @@
 var express = require('express');
+const { requiresAuth } = require('express-openid-connect');
 var router = express.Router();
 
-// Homepage
-router.get('/', function(req, res) {
-    res.render("index.html");
+// Locals used in multiple pages
+router.use((req, res, next) => {
+    res.locals.isAuthenticated = req.oidc.isAuthenticated();
+    res.locals.activeRoute = req.originalUrl;
+    next(); 
 });
 
-router.get('/rocks', function(req, res) {
-    res.render("rocks.html")
-})
+// Sign-in and Sign-out Routes
+router.get('/login/:page', (req, res) => {
+    let page = req.params.page;
+
+    res.oidc.login({
+        returnTo: page,
+    });
+});
+
+router.get('/sign-up/:page', (req, res) => {
+    let page = req.params.page;
+
+    res.oidc.login({
+        returnTo: page,
+        authorizationParams: {
+            screen_hint: "signup",
+        },
+    });
+});
+
+router.get('/logout/:page', (req, res) => {
+    let page = req.params.page;
+    
+    res.oidc.logout({
+        returnTo: page,
+    });
+});
+
+// Regular Routes
+router.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+router.get('/profile', (req, res) => {
+    res.render("profile.html");
+});
+
+router.get('/rocks', requiresAuth(), (req, res) => {
+    res.render("rocks.html");
+});
 
 module.exports = router;
