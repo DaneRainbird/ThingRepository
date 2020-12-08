f6e2f4db57b03edaa8bc2eac9d71188980745711
drai0001 <13450157+Datenyan@users.noreply.github.com>
2020-12-08 17:19:38 +1100

    Added mongodb connection for users

--- router.js
+++ router.js
@@ -1,6 +1,38 @@
 var express = require('express');
 const { requiresAuth } = require('express-openid-connect');
+let mongoose = require('mongoose');
 var router = express.Router();
+
+let justSignedUp = false;
+
+// MongoDB variables and connection
+let url = "mongodb://localhost:27017/data";
+let User = require('./models/users.js');
+mongoose.connect(url, {useNewUrlParser: true}, function(err, client) {
+    if (err) {
+        console.log("Could not connect to DB");
+    } else {
+        console.log("Connected to DB at " + url);
+    }
+});
+
+// Helper Functions
+let createUserInDb = function(userEmail) {
+    User.findOne({email: userEmail }, function (err, user) {
+        if (err) throw err;
+        if (!user) {
+            let newUser = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: userEmail
+            })
+            newUser.save(function(err) {
+                if (err) throw err;
+            });
+        } else {
+            console.log("user already exists.");
+        }
+    });
+}
 
 // Locals used in multiple pages
 router.use((req, res, next) => {
@@ -9,7 +41,7 @@ router.use((req, res, next) => {
     next(); 
 });
 
-// Sign-in and Sign-out Routes
+// Sign-in,  Sign-out, and Sign-up Routes
 router.get('/login/:page', (req, res) => {
     let page = req.params.page;
 
@@ -27,6 +59,7 @@ router.get('/sign-up/:page', (req, res) => {
             screen_hint: "signup",
         },
     });
+    justSignedUp = true;
 });
 
 router.get('/logout/:page', (req, res) => {
@@ -37,13 +70,22 @@ router.get('/logout/:page', (req, res) => {
     });
 });
 
+
 // Regular Routes
 router.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    // TODO - create landing page. for now, redirect to profile page
+    res.redirect("profile");
 });
 
 router.get('/profile', (req, res) => {
-    res.render("profile.html");
+    if (justSignedUp) {
+        // Create a user on sign up
+        createUserInDb(req.oidc.user.name);
+        justSignedUp = false;
+    }
+
+    let username = req.oidc.isAuthenticated() ? req.oidc.user.name : '';
+    res.render("profile.html", {user: username});
 });
 
 router.get('/rocks', requiresAuth(), (req, res) => {
